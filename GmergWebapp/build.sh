5457b5913e5e9accdddb023124895a6f4ed94601
yiya <yiya.yang@igmm.ed.ac.uk>
2012-06-22 13:22:00 +0100

    add debug flag, bean name = class name

--- GmergWebapp/build.sh
+++ GmergWebapp/build.sh
@@ -1,12 +1,6 @@
 #! /bin/csh
 # build -- Build script for LocalServer deployment
 
-# Show what deployment is being built.  Real purpose is to abort if 
-# $CONFIGURATION is not defined
-echo
-echo "Building ==> $CONFIGURATION <=="
-echo
-
 # set java and tomcat environment variables
 setenv JAVA_HOME '/opt/java'
 setenv CATALINA_HOME '/export/data0/tomcat'
