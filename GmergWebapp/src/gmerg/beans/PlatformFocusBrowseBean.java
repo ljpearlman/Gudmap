583ed5c93897333bb621e11016b234717ad2a655
yiya <yiya.yang@igmm.ed.ac.uk>
2012-06-22 13:23:18 +0100

    add debug flag, bean name = class name

--- GmergWebapp/src/gmerg/beans/PlatformFocusBrowseBean.java
+++ GmergWebapp/src/gmerg/beans/PlatformFocusBrowseBean.java
@@ -7,13 +7,17 @@ import gmerg.utils.table.*;
 import gmerg.utils.Visit;
 
 public class PlatformFocusBrowseBean {
-	
+    private boolean debug = false;
+
 	private String organ;
 	
 	// ********************************************************************************
 	// Constructors & Initializers
 	// ********************************************************************************
 	public PlatformFocusBrowseBean() {
+	if (debug)
+	    System.out.println("PlatformFocusBrowseBean.constructor");
+
 		organ = Visit.getRequestParam("focusedOrgan");
 		String viewName = "platformFocusBrowse";
 		if (TableUtil.isTableViewInSession())
