ec1f7ad715fe2c4997b0bb3b5bacb73e3e316429
yiya <yiya.yang@igmm.ed.ac.uk>
2012-06-22 13:23:11 +0100

    add debug flag, bean name = class name

--- GmergWebapp/src/gmerg/beans/MaProbeBean.java
+++ GmergWebapp/src/gmerg/beans/MaProbeBean.java
@@ -5,11 +5,15 @@ import gmerg.utils.FacesUtil;
 import gmerg.assemblers.MaProbeAssembler;
 
 public class MaProbeBean {
-	
+    private boolean debug = false;
+
 	private Probe maProbe;
 	private MaProbeAssembler assembler;
 	
 	public MaProbeBean() {
+	if (debug)
+	    System.out.println("MaProbeBean.constructor");
+
 		String probeId = FacesUtil.getRequestParamValue("probe");
 		String maprobeId = FacesUtil.getRequestParamValue("maprobe");
 		
